4913dfc88f3d4a3bf91ca1a5742039d6ad6dc7e3
puppy0cam <7194673+puppy0cam@users.noreply.github.com>
2017-10-06 12:46:44 +1000

    Create SSR.meta.js

---
+++ SSR.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             portal-status-slack@puppy0cam
+// @name           IITC plugin: SSR - Slack Status Reports
+// @category       Controls
+// @version        0.0.7.1
+// @namespace      https://github.com/puppy0cam/portal-status
+// @description    Create a status report to copy/paste into slack | backend by puppy0cam | formatting by aladrin
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// @updateURL      https://puppy0cam.github.io/SSR/SSR.meta.js
+// @downloadURL    https://puppy0cam.github.io/SSR/SSR.user.js
+// ==/UserScript==
